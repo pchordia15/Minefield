11e73c8b8616f4e7ab81fc1ca4cca29f03726396
Priyanka Chordia <pchordia@mail.csuchico.edu>
2016-03-09 10:22:29 -0800

    Update FieldTest.cpp

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -23,82 +23,10 @@ TEST(FieldTest, placeMineInBounds)
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
 
-TEST(FieldTest, checkMineHidden)
+TEST(FieldTest, isSafeMineExplodes)
 {
-	Field minefiled;
-	ASSERT_FALSE(minefield.isSafe(4,5));
-}
-
-TEST(FieldTest, checkMineNotHidden)
-{
-	Field minefield;
-	minefield.placeMine(4,5);
-	ASSERT_TRUE(minefield.isSafe(5,5));
-}
-
-TEST(FieldTest, checkMineOutOfX)
-{
-	Field minefield;
-	bool flag = FALSE;
-	minefield.placeMine(4,5);
-	try
-	{
-		minefield.isSafe(12,5);
-	}
-	catch(...)
-	{
-		flag = true;	
-	}
-	ASSERT_TRUE(flag);
-}
-
-TEST(FieldTest, checkMineOutOfy)
-{
-	Field minefield;
-	bool flag = FALSE;
-	minefield.placeMine(4,5);
-	try
-	{
-		minefield.isSafe(4,12);
-	}
-	catch(...)
-	{
-		flag = true;	
-	}
-	ASSERT_TRUE(flag);
-}
-
-TEST(FieldTest, checkAdjacent)
-{
-	Field minefield;
-	minefield.placeMine(4,5);
-	ASSERT_EQ( EMPTY_SHOWN, minefield.revealAdjacent(4,5));
-}
-
-TEST(FieldTest, checkAdjacentup)
-{
-	Field minefield;
-	minefield.placeMine(4,5);
-	ASSERT_EQ( EMPTY_SHOWN, minefield.revealAdjacent(3,5));
-}
-
-TEST(FieldTest, checkAdjacentdown)
-{
-	Field minefield;
-	minefield.placeMine(4,5);
-	ASSERT_EQ( EMPTY_SHOWN, minefield.revealAdjacent(5,5));
-}
-
-TEST(FieldTest, checkAdjacentleft)
-{
-	Field minefield;
-	minefield.placeMine(4,5);
-	ASSERT_EQ( EMPTY_SHOWN, minefield.revealAdjacent(4,4));
-}
-
-TEST(FieldTest, checkAdjacentright)
-{
-	Field minefield;
-	minefield.placeMine(4,5);
-	ASSERT_EQ( EMPTY_SHOWN, minefield.revealAdjacent(4,6));
+	Field oneMine;
+	
+	oneMine.placeMine(1,2);
+	ASSERT_FALSE(oneMine.isSafe(1,2));
 }
